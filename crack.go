5310c31f21c6feacd92b4686dce3251922a24886
Jacob Strieb <jstrieb@users.noreply.github.com>
2021-04-23 02:44:23 -0400

    Refactor with clean helper functions

--- crack.go
+++ crack.go
@@ -20,10 +20,12 @@ type DataObject struct {
 	// Version   string `json:"v"`
 }
 
-func main() {
+// ParseUrl extracts a base64 encoded JSON string from the fragment of a URL, if
+// possible. Otherwise it raises fatal errors.
+func ParseUrl(rawUrl string) *DataObject {
 	// Parse URL
 	// NOTE: Test password is "test" and destination is https://jstrieb.github.io/link-lock/create/
-	url, err := url.Parse("https://jstrieb.github.io/link-lock/#eyJ2IjoiMC4wLjEiLCJlIjoiWEk0ZS9GQkcxcko5Y1JCRVovUzk4Sk5IeGJwN0ljRk5MZUhTcVNrTUlpbW1mOFp4WlJIclEyK0lmY1liY3hOKy84WmlhMHdQYWFxcFhOcz0iLCJzIjoiMlFDNkIrcHcxckw4S0RiV1MvdWZqZz09IiwiaSI6IndCYkZZdHI4UFlNUittYnQifQ==")
+	url, err := url.Parse(rawUrl)
 	if err != nil {
 		log.Fatal("Unable to parse the URL.")
 	}
@@ -40,14 +42,21 @@ func main() {
 		log.Fatal("Failed to unmarshal JSON.")
 	}
 
+	return data
+}
+
+// Try to decrypt the encrypted data object with the password. Return the
+// plaintext if decrypted, and a boolean status value. Fail catastrophically if
+// any parsing step fails.
+func TryDecrypt(password string, data *DataObject) (string, bool) {
 	// Perform key derivation
 	salt, err := base64.StdEncoding.DecodeString(data.Salt)
 	if err != nil {
 		log.Fatal("Failed to base64 decode the salt.")
 	}
-	k := pbkdf2.Key([]byte("test"), salt, 100000, 32, sha256.New)
+	k := pbkdf2.Key([]byte(password), salt, 100000, 32, sha256.New)
 
-	// Generate an AES decoder
+	// Generate an AES decoder for the given key
 	block, err := aes.NewCipher(k)
 	if err != nil {
 		log.Fatal("Failed to create a new cipher from the key.")
@@ -70,8 +79,18 @@ func main() {
 	// Decrypt ciphertext
 	plaintext, err := aesgcm.Open(nil, iv, encrypted, nil)
 	if err != nil {
-		log.Fatal("Decoding failed!")
+		return "", false
 	}
 
+	return string(plaintext), true
+}
+
+func main() {
+	data := ParseUrl("https://jstrieb.github.io/link-lock/#eyJ2IjoiMC4wLjEiLCJlIjoiWEk0ZS9GQkcxcko5Y1JCRVovUzk4Sk5IeGJwN0ljRk5MZUhTcVNrTUlpbW1mOFp4WlJIclEyK0lmY1liY3hOKy84WmlhMHdQYWFxcFhOcz0iLCJzIjoiMlFDNkIrcHcxckw4S0RiV1MvdWZqZz09IiwiaSI6IndCYkZZdHI4UFlNUittYnQifQ==")
+
+	plaintext, ok := TryDecrypt("test", data)
+	if !ok {
+		fmt.Println("Decryption failed!")
+	}
 	fmt.Println(string(plaintext))
 }
